da0d36a60e14ab195e5c2456e00e56b0e1caabfd
ritikdevelop <hrithikrana216@gmail.com>
2026-03-26 12:26:40 +0530

    update the sql query

--- Task/setup.sql
+++ Task/setup.sql
@@ -1,18 +1,6 @@
---
--- Database: `task_manager`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tasks`
---
-
-CREATE TABLE `tasks` (
-  `id` int(11) NOT NULL,
-  `task_name` varchar(255) NOT NULL,
-  `status` varchar(20) DEFAULT 'Created' CHECK (`status` in ('Created','WIP','Completed')),
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
-
+CREATE TABLE IF NOT EXISTS tasks (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    task_name VARCHAR(255) NOT NULL,
+    status VARCHAR(20) DEFAULT 'Created' CHECK (status IN ('Created', 'WIP', 'Completed')),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
