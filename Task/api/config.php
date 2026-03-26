21ec8d25d7d8507278ef5ab6d31bd85a8b6375ad
ritikdevelop <hrithikrana216@gmail.com>
2026-03-26 12:24:23 +0530

    update the config file

--- Task/api/config.php
+++ Task/api/config.php
@@ -5,11 +5,12 @@
  * In a production environment, these should be managed via Environment Variables.
  */
 
-define('DB_HOST', getenv('DB_HOST') ?: 'localhost');
+define('DB_HOST', getenv('DB_HOST') ?: 'sql212.infinityfree.com');
 define('DB_PORT', getenv('DB_PORT') ?: '3306');
-define('DB_NAME', getenv('DB_NAME') ?: 'task_manager');
-define('DB_USER', getenv('DB_USER') ?: 'root');
-define('DB_PASS', getenv('DB_PASSWORD') ?: '');
+define('DB_NAME', getenv('DB_NAME') ?: 'if0_41480613_task_manager
+');
+define('DB_USER', getenv('DB_USER') ?: 'if0_41480613');
+define('DB_PASS', getenv('DB_PASSWORD') ?: 'erlrqrmjiXa3uyE');
 
 /**
  * Display errors during development. 
